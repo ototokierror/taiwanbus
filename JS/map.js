d60261675c3332711c3062526986a1689d69c6de
ototoki.error@gmail.com <ototoki.error@gmail.com>
2021-11-16 18:24:26 +0800

    update 11/16 18:24

--- JS/map.js
+++ JS/map.js
@@ -1,7 +1,5 @@
 // city ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
 const cityData = [
-  { name: '臺北市', value: 'Taipei' },
-  { name: '新北市', value: 'NewTaipei' },
   { name: '桃園市', value: 'Taoyuan' },
   { name: '臺中市', value: 'Taichung' },
   { name: '臺南市', value: 'Tainan' },
@@ -26,10 +24,10 @@ const cityData = [
 const citySelect = document.getElementById('citySelect');
 const routeSelector = document.getElementById('routeSelector');
 
-function citySelector(){
+function citySelector() {
   let str = '';
   cityData.forEach(i => {
-    str+= `<option value="${i.value}">${i.name}</option>`
+    str += `<option value="${i.value}">${i.name}</option>`
   });
   citySelect.innerHTML = str;
 }
@@ -38,25 +36,25 @@ let cityName = '';
 let routeName = '';
 
 // 監聽 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-citySelect.addEventListener('change',function(e){
+citySelect.addEventListener('change', function (e) {
   console.log(e.target.value);
   ruteSearch(e.target.value);
-  cityName = e.target.value ;
+  cityName = e.target.value;
 })
-routeSelector.addEventListener('change',function(e){
+routeSelector.addEventListener('change', function (e) {
   routeName = e.target.value;
-  routeSearch ();
+  routeSearch();
 })
 
 // MAP ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-let mymap = L.map('mapid').setView([35.6896067,139.6983773], 11);
+let mymap = L.map('mapid').setView([35.6896067, 139.6983773], 11);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 20,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoic29ueWtvIiwiYSI6ImNrdmFvNDI3MzJxMGUydW5uMjE0dWd1MnkifQ.d5Tb0HYCecMQxpqq_YivNQ'
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 20,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1Ijoic29ueWtvIiwiYSI6ImNrdmFvNDI3MzJxMGUydW5uMjE0dWd1MnkifQ.d5Tb0HYCecMQxpqq_YivNQ'
 }).addTo(mymap);
 
 // 定位 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
@@ -77,98 +75,134 @@ if (navigator.geolocation) {
     function (e) {
       const msg = e.code;
       const dd = e.message;
-       console.error(msg)
-       console.error(dd)
+      console.error(msg)
+      console.error(dd)
     }
   )
 }
 // TDX ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-const busData = [] ;
+const busData = [];
 const appId = 'd97e3a7603f049728fb4966c08eba8da';
 const appKey = '2mG7iZNmoX2fvkOpn2XQauzJMqI';
 const APIURL = 'https://ptx.transportdata.tw/MOTC';
 // 取得城市 and 車車 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-function ruteSearch(cityName){
+function ruteSearch(cityName) {
   let routeNameArr = [];
   axios({
     method: 'get',
     url: `${APIURL}/v2/Bus/RealTimeByFrequency/City/${cityName}`,
     headers: getAuthorizationHeader()
   })
-   .then(function (response) {
-     let busData ;
-     busData = response.data;
-     busData.forEach(i=>{
-      routeNameArr.push(i.RouteName.Zh_tw);
-     })
-     let newRouteNameArr = routeNameArr.reduce((obj, item)=> {
-       obj[item] = 1 ;
-       return obj ;
-     },{});
-     newRouteNameArr = Object.keys(newRouteNameArr);
-     let str = '';
-     newRouteNameArr.forEach(i => {
-       str += `<option value="${i}">${i}</option>`;
-     });
-     routeSelector.innerHTML = str;
-   })
-   .catch(function (error) {
-     console.log(error);
-   }); 
+    .then(function (response) {
+      let busData;
+      busData = response.data;
+      console.log(busData);
+      busData.forEach(i => {
+        routeNameArr.push(i.RouteName.Zh_tw);
+      })
+      let newRouteNameArr = routeNameArr.reduce((obj, item) => {
+        obj[item] = 1;
+        return obj;
+      }, {});
+      newRouteNameArr = Object.keys(newRouteNameArr);
+      newRouteNameArr.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', { sensitivity: 'accent' }));
+      let str = '';
+      newRouteNameArr.forEach(i => {
+        str += `<option value="${i}">${i}</option>`;
+      });
+      routeSelector.innerHTML = str;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 const departureStopName = document.getElementById('departureStopName');
 const destinationStopName = document.getElementById('destinationStopName');
 const busDirection = document.getElementById('busDirection');
-let backData= [] ;
+const stopSeqContent = document.getElementById('stopSeqContent');
 
+
+let backData = [];
+let goData = [];
 // 搜尋選擇的車車 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-function routeSearch () {
-  const getbusURL =  `${APIURL}/v2/Bus/EstimatedTimeOfArrival/City/${cityName}/${routeName}`;  
+function routeSearch() {
+  const getbusURL = `${APIURL}/v2/Bus/EstimatedTimeOfArrival/City/${cityName}/${routeName}`;
   axios({
     method: 'get',
     url: getbusURL,
     headers: getAuthorizationHeader()
   })
-  .then (res => {
-    let data = res.data;
-    console.log('全部的車車', data);
-    // 將沒有跑的車車篩掉 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-    let bus = data.filter(i => i.StopStatus===0);
-    console.log('有開的車車', bus);
-    // 去程車車 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-    let catchGoData = bus.filter(i => !i.Direction);
-  
-    // 返程車車 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
-    let catchBackData = bus.filter(i => i.Direction);
-    console.log('去程車車：',catchGoData,'返程車車',catchBackData);
-    
-    catchBackData.forEach((i) => { 
-      const index = backData.map(i => i.plateNumb).indexOf(i.PlateNumb);
-     
-      if (index === -1) { // 代表沒找到
-        backData.push({
-          plateNumb: i.PlateNumb, //車牌號碼
-          stops: [{
+    .then(res => {
+      let data = res.data;
+      console.log('全部的車車', data);
+      // 將沒有跑的車車篩掉 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
+      let bus = data.filter(i => i.StopStatus === 0);
+      console.log('有開的車車', bus);
+      // 去程車車 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
+      let catchGoData = bus.filter(i => !i.Direction);
+      console.log('去程車車：', catchGoData);
+
+      catchGoData.forEach(i => {
+        const index = goData.map(i => i.plateNumb).indexOf(i.PlateNumb);
+        if (index === -1) { // 代表沒找到
+          goData.push({
+            plateNumb: i.PlateNumb, //車牌號碼
+            stops: [{
+              estimateTime: i.EstimateTime,//到站時間預估(秒) 
+              stopUID: i.StopUID//站牌唯一識別代碼
+            }]
+          })
+        } else { // 有找到
+          goData[index].stops.push({
             estimateTime: i.EstimateTime,//到站時間預估(秒) 
             stopUID: i.StopUID//站牌唯一識別代碼
-          }]
-        })
-      } else { // 有找到
-        backData[index].stops.push({
-          estimateTime: i.EstimateTime,//到站時間預估(秒) 
-          stopUID: i.StopUID//站牌唯一識別代碼
-        });
+          });
+        }
+      })
+
+      // 返程車車 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
+      let catchBackData = bus.filter(i => i.Direction);
+      console.log('返程車車', catchBackData);
+
+      catchBackData.forEach((i) => {
+        const index = backData.map(i => i.plateNumb).indexOf(i.PlateNumb);
+
+        if (index === -1) { // 代表沒找到
+          backData.push({
+            plateNumb: i.PlateNumb, //車牌號碼
+            stops: [{
+              estimateTime: i.EstimateTime,//到站時間預估(秒) 
+              stopUID: i.StopUID//站牌唯一識別代碼
+            }]
+          })
+        } else { // 有找到
+          backData[index].stops.push({
+            estimateTime: i.EstimateTime,//到站時間預估(秒) 
+            stopUID: i.StopUID//站牌唯一識別代碼
+          });
+        }
+
+      })
+      getbusStatus();
+      getRoute();
+
+      if (goData.length == 0) {
+        console.log('去程沒車車')
+      } else {
+        console.log('整理過的去程車車', goData)
       }
-      
+
+      if (backData.length == 0) {
+        console.log('返程沒車車')
+      } else {
+        console.log('整理過的返程車車', backData)
+      }
+
     })
-    getbusStatus();
-    getRoute();
-   console.log('整理過的返程車車', backData)
-  })
-  .catch(err=>{
-    console.log(err);
-  })
+    .catch(err => {
+      console.log(err);
+    })
 }
 function getRoute() {
   const URL = `${APIURL}/v2/Bus/StopOfRoute/City/${cityName}/${routeName}`;
@@ -177,67 +211,102 @@ function getRoute() {
     url: URL,
     headers: getAuthorizationHeader()
   })
-  .then(res => {
-    const data = res.data;
-    const routeData = data.filter((item) => item.RouteName.Zh_tw === routeName);
-    console.log('往返列表', routeData);
-    // 去程
-    // let gobusID = ''
-    //       let gotime = 0;
-    //       let gotimeText = '';
-    // console.log('往資料', routeData[0])
-    //       routeData[0].Stops.forEach((item) => {
-    //         backData.forEach((go) => {
-    //           go.stops.forEach((stop) => {
-    //             if (stop.stopUID === item.StopUID) {
-    //               gobusID = go.plateNumb
-    //               gotime = Math.floor(stop.estimateTime / 60)
-    //               console.log('往',gobusID, gotime)
-    
-    //               // 文字顯示
-    //               if (gotime === 0) {
-    //                 gotimeText = '進站中';
-    //               } else if (gotime <= 1 && 0 < gotime) {
-    //                 gotimeText = '即將到站';
-    //               } else if (!gotime) {
-    //                 gotimeText = '--';
-    //               } else {
-    //                 gotimeText = `${gotime} 分鐘`;
-    //               }
-    //             }
-    //           })
-    //         })
-    //       })
-          
-          // 返程
-          let backbusID = ''
-          let backtime = 0;
-          let backtimeText = '';
-    
-          routeData[1].Stops.forEach((item) => {
-            backData.forEach((back) => {
-              back.stops.forEach((stop) => {
-                if (stop.stopUID === item.StopUID) {
-                  backbusID = back.plateNumb
-                  backtime = Math.floor(stop.estimateTime / 60)
-                  console.log('返',backbusID, backtime)
-    
-                  // 文字顯示
-                  if (backtime === 0) {
-                    backtimeText = '進站中';
-                  } else if (backtime <= 1 && 0 < backtime) {
-                    backtimeText = '即將到站';
-                  } else if (!backtime) {
-                    backtimeText = '--';
-                  } else {
-                    backtimeText = `${backtime} 分鐘`;
-                  }
-                }
-              })
-            })
+    .then(res => {
+      const data = res.data;
+      const routeData = data.filter((item) => item.RouteName.Zh_tw === routeName);
+      console.log('往返列表', routeData);
+      // 去程 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
+      let gobusID = ''
+      let gotime = 0;
+      let gotimeText = '';
+
+      routeData[0].Stops.forEach((item) => {
+        goData.forEach((go) => {
+          go.stops.forEach((stop) => {
+            if (stop.stopUID === item.StopUID) {
+              gobusID = go.plateNumb
+              gotime = Math.floor(stop.estimateTime / 60)
+              console.log('往', gobusID, gotime)
+
+              // 文字顯示
+              if (gotime === 0) {
+                gotimeText = '進站中';
+              } else if (gotime <= 1 && 0 < gotime) {
+                gotimeText = '即將到站';
+              } else if (!gotime) {
+                gotimeText = '--';
+              } else {
+                gotimeText = `${gotime} 分鐘`;
+              }
+            }
           })
-  })
-  .catch(err => { console.log(err) })
+        })
+      })
+
+      // 返程 ★∻∹⋰⋰ ☆∻∹⋰⋰ ★∻∹⋰⋰ ☆∻∹⋰⋰
+      let backbusID = ''
+      let backtime = 0;
+      let backtimeText = '';
+      let backStr = '';
+
+      routeData[1].Stops.forEach((item, index) => {
+        backData.forEach((back) => {
+          back.stops.forEach((stop) => {
+            if (stop.stopUID === item.StopUID) {
+              backbusID = back.plateNumb
+              backtime = Math.floor(stop.estimateTime / 60)
+              console.log('返', backbusID, backtimeText)
+
+              // 文字顯示
+              if (backtime === 0) {
+                backtimeText = '進站中';
+              } else if (backtime <= 1 && 0 < backtime) {
+                backtimeText = '即將到站';
+              } else if (!backtime) {
+                backtimeText = '--';
+              } else {
+                backtimeText = `${backtime} 分鐘`;
+              }
+            }
+          })
+        })
+        backStr += `
+<div class="seq-content">
+<p class="w32 center">${index + 1}</p>
+<p class="w216 df-aic">${item.StopName.Zh_tw}
+  <span class="material-icons place">place</span>
+</p>
+<p class="w64">${backtimeText}</p>
+</div>`
+      })
+      let newStr = `
+      <h3
+      class="price_search"
+      id="myBtn"
+    >票價查詢</h3>
+      <ul class="bus-realtime">
+      <li>
+        <div class="sequence">
+          <p class="w32">站序</p>
+          <p class="w216">站名</p>
+          <p class="w64">預估到站</p>
+        </div>
+      </li>
+      ${backStr}
+      </ul>
+      <div class="update-content">
+      <ul class="update">
+        <li><span id="updateCountdown">30</span> 秒後更新</li>
+        <li id="update">更新</li>
+      </ul>
+    </div>
+      `
+      stopSeqContent.innerHTML = newStr;
+    })
+    .catch(err => {
+      console.log(err)
+      alert('目前無此車車')
+    })
 }
 
 
@@ -249,30 +318,30 @@ function getbusStatus() {
     url: getRouteURL,
     headers: getAuthorizationHeader()
   })
-  .then (res => {
-    getbusData = res.data ;
-    let newGetbusData = getbusData.filter(i => i.RouteName.Zh_tw=== routeName);
-    console.log(newGetbusData)
-    busDirection.innerHTML = `
+    .then(res => {
+      getbusData = res.data;
+      let newGetbusData = getbusData.filter(i => i.RouteName.Zh_tw === routeName);
+
+      busDirection.innerHTML = `
     <li class="bdrs_left">往 <span>${newGetbusData[0].DepartureStopNameZh}</span></li>
     <li class="bdrs_right">往 <span>${newGetbusData[0].DestinationStopNameZh}</span></li>
     `
-  })
-  .catch(err=>{
-    console.log(err);
-  })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 
 function getAuthorizationHeader() {
-      let AppID = appId;
-      let AppKey = appKey;
-      let GMTString = new Date().toGMTString();
-      let ShaObj = new jsSHA('SHA-1', 'TEXT');
-      ShaObj.setHMACKey(AppKey, 'TEXT');
-      ShaObj.update('x-date: ' + GMTString);
-      let HMAC = ShaObj.getHMAC('B64');
-      let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
-      return { 'Authorization': Authorization, 'X-Date': GMTString }; 
-  }
-  
+  let AppID = appId;
+  let AppKey = appKey;
+  let GMTString = new Date().toGMTString();
+  let ShaObj = new jsSHA('SHA-1', 'TEXT');
+  ShaObj.setHMACKey(AppKey, 'TEXT');
+  ShaObj.update('x-date: ' + GMTString);
+  let HMAC = ShaObj.getHMAC('B64');
+  let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+  return { 'Authorization': Authorization, 'X-Date': GMTString };
+}
+
